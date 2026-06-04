8c5d1eafd04a62179d9eb3a5eb0809d9e0ac6710
JoaoMoreiraLudin <joaokingu@gmail.com>
2026-06-04 20:14:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-async function atualizarContador() {
-    const resposta = await fetch(
-        "https://projetomes-31cea-default-rtdb.firebaseio.com/maquinas/injetora_01/pecas.json"
-    );
-
-    const pecas = await resposta.json();
-
-    console.log("Valor recebido:", pecas);
-
-    document.getElementById("contador").innerText = pecas;
-}
-
-atualizarContador();
+document.getElementById("contador").innerText = "999";
+console.log("Script carregado!");
